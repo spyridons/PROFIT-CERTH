1a70ec4bb4eb38009ec1b37fd4ed5a92a2f7ddc9
Spyridon Symeonidis <spyridons@hotmail.com>
2017-12-14 12:12:24 +0200

    Added license info to the source code files

--- recommender_system/service/recommender.py
+++ recommender_system/service/recommender.py
@@ -1,3 +1,18 @@
+# Copyright 2017 Project PROFIT (http://projectprofit.eu/)
+#
+# Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+# the European Commission - subsequent versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the Licence.  You may
+# obtain a copy of the Licence at:
+#
+# https://joinup.ec.europa.eu/software/page/eupl
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Licence is distributed on an "AS IS" basis, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# Licence for the specific language governing permissions and limitations
+# under the Licence.
+
 from scipy.sparse import lil_matrix, csr_matrix
 from enum import Enum
 import numpy as np
